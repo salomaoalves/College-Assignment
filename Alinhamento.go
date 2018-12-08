72bdfcda966627abc0b849f81a4edba49f262c28
Salomao <salomao@salomao.alves222@gmail.com>
2018-12-08 18:32:57 -0200

    pouca coisa feita

---
+++ Alinhamento.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt";
+	"os";
+	"bufio"
+)
+
+func main(){
+	//recebe as inf
+	inputUtilizador := bufio.NewReader(os.Stdin)
+	fmt.Print("Digite a sequencia A:")
+	seq1 := inputUtilizador.ReadeString('\n')
+	fmt.Print("\nDigite a sequencia B:")
+	seq2 := inputUtilizador.ReadeString('\n')
+	fmt.Print("Digite o gap:")
+	d := inputUtilizador.ReadeString('\n')
+	size1 := len(seq1)
+	size2 := len(seq2)
+
+	//criando as matrizes
+	tabela := [seq1+1][seq2+1] int
+
+	//inicializacao
+
+	for i:=0; i<=size1; i++{
+		for j:=0; j<=size2; j++{
+			F := condIni(i,j,d)
+			//add F na tabela
+		}
+	}
+
+	//preenchimento (scoring)
+	for i:=1; i<=size1; i++{
+		for j:=1; j<=size2; j++{
+			F := pontuacao(i,j,d)
+			//add F na tabela
+		}
+	}
+
+	//alinhamento(traceback)
+}
+
+func condIni(i int, j int, d int) int{
+	if i == 0 && j == 0{
+		return 0
+	}else{
+		if j == 0{
+			return -i * d
+		}else{
+			return -j * d
+		}
+	}
+}
+
+func pontuacao(i int, j int, d int) int{
+	l1 := F(i-1,j-1) + s(i,j)
+	l2 := F(i-1,j) - d
+	l3 := F(i,j-1) - d 
+	return max(l1,l2,l3)
+}
+
+func F(i int, j int) int{
+	//procurar na propria matriz
+}
+
+func s(i int, j int) int{
+	//procurar na matriz de merito
+}
+
+func max(l1 int, l2 int, l3 int) int{
+	if l1 > l2 && l1 > l3 {
+		return l1
+	}else{
+		if l2 > l3{
+			return l2
+		}else{
+			return l3
+		}
+	}
+}
